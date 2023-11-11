ca2bce2ba8b4704daa63b971ecfe4815668d7795
Iago Mendonça <beckeriago83@gmail.com>
2023-11-11 10:29:14 -0300

    feat: add all project and first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import HeroImage from "../assets/heroImage.png";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+
+const Home = () => {
+  return (
+    <div
+      name="home"
+      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-700"
+    >
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+        <div className="flex flex-col justify-center h-full">
+          <h2 className="text-4xl sm:text-7xl font-bold text-white">
+            Eu sou Full Stack Developer
+          </h2>
+          <p className="text-gray-500 py-4 max-w-md">
+            Sou desenvolvedor web com experiência mais voltada para o backend.
+            Tenho habilidades sólidas em desenvolvimento de aplicações web e
+            estou sempre em busca de aprender novas tecnologias para me
+            aperfeiçoar. Atualmente, adoro trabalhar em aplicações web usando
+            tecnologias como Node.js | Nest.js | React.js | Javascript |
+            Typescript | MySQL | MongoDB | Tailwind
+          </p>
+          <div>
+            <button className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-950 cursor-pointer">
+              Portfólio
+              <span className="group-hover:rotate-90 duration-300">
+                <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
+              </span>
+            </button>
+          </div>
+        </div>
+
+        <div>
+          <img
+            src={HeroImage}
+            alt="meu perfil"
+            className="rounded-2xl mx-auto w-2/3 md:w-200"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,113 @@
+import React from "react";
+
+import arrayDestruct from "../assets/portfolio/arrayDestruct.jpg";
+import installNode from "../assets/portfolio/installNode.jpg";
+import navbar from "../assets/portfolio/navbar.jpg";
+import reactParallax from "../assets/portfolio/reactParallax.jpg";
+import reactSmooth from "../assets/portfolio/reactSmooth.jpg";
+import reactWeather from "../assets/portfolio/reactWeather.jpg";
+
+const Portfolio = () => {
+  const portfolios = [
+    {
+      id: 1,
+      src: arrayDestruct,
+      demoURL:
+        "https://user-images.githubusercontent.com/83254191/276341236-e0fcd992-9c9e-4129-94c0-ae417c769d2b.jpg",
+      codeURL: "https://github.com/darkbecker4-97/AppHotelReactNative.git",
+    },
+    {
+      id: 2,
+      src: installNode,
+      demoURL: "https://portfolioiagobecker.netlify.app",
+      codeURL: "https://github.com/darkbecker4-97/Potfolio.git",
+    },
+    {
+      id: 3,
+      src: navbar,
+      demoURL: "https://github.com/darkbecker4-97/Bootstrap2.0.git",
+      codeURL: "https://github.com/darkbecker4-97/Bootstrap2.0.git",
+    },
+    {
+      id: 4,
+      src: reactParallax,
+      demoURL: "https://github.com/darkbecker4-97/finance-control.git",
+      codeURL: "https://github.com/darkbecker4-97/finance-control.git",
+    },
+    {
+      id: 5,
+      src: reactSmooth,
+      demoURL: "https://github.com/darkbecker4-97/ServicosMDark.git",
+      codeURL: "https://github.com/darkbecker4-97/ServicosMDark.git",
+    },
+    {
+      id: 6,
+      src: reactWeather,
+      demoURL: "https://github.com/darkbecker4-97/LandingPageProt.git",
+      codeURL: "https://github.com/darkbecker4-97/LandingPageProt.git",
+    },
+  ];
+
+  const openLinkInNewTab = (url) => {
+    window.open(url, "_blank");
+  };
+
+  return (
+    <div
+      name="portfolio"
+      className="bg-gradient-to-b from-black to-gray-700 w-full text-white md:h-screen"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Portfólio
+          </p>
+          <p className="py-6">Confira alguns dos meus trabalhos aqui</p>
+        </div>
+
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+          {portfolios.map(({ id, src, demoURL, codeURL }) => (
+            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  height: "200px",
+                }}
+              >
+                <img
+                  src={src}
+                  alt=""
+                  className="rounded-md duration-200 hover:scale-105"
+                  style={{
+                    maxWidth: "300px",
+                    maxHeight: "200px",
+                    width: "auto",
+                    height: "auto",
+                  }}
+                />
+              </div>
+              <div className="flex items-center justify-center">
+                <button
+                  className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"
+                  onClick={() => demoURL && openLinkInNewTab(demoURL)}
+                >
+                  Demonstração
+                </button>
+                <button
+                  className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"
+                  onClick={() => codeURL && openLinkInNewTab(codeURL)}
+                >
+                  Código
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
